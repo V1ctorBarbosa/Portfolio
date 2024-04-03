bc6f99ff6c07d0b37a3cbd91f2bbc4f9fb9af705
V1ctorBarbosa <vicfer2708@gmail.com>
2024-04-02 23:35:09 -0300

    style: contact animation

--- src/sections/contact/contact.tsx
+++ src/sections/contact/contact.tsx
@@ -1,3 +1,6 @@
+//React
+import { useEffect } from "react";
+
 //Styles
 import { colors } from "../../styles/colors";
 import { Container, ContactsSection } from "./contact.styles";
@@ -11,10 +14,34 @@ import { handleContacts } from "./utils";
 //Components
 import Text from "../../components/Text/text";
 
+//Animation
+import { useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { onViewAnimation } from "../../styles/animation";
+
+//Types
+import { IOnViewAnimation } from "../../styles/types";
+
 function Contact() {
 
+  const controls = useAnimation();
+  const { ref, inView } = useInView();
+
+  const animationVariants: IOnViewAnimation = onViewAnimation("-5%");
+
+    useEffect(() => {
+      if (inView) {
+        controls.start("visible");
+      }
+    }, [inView])
+
   return (
-    <Container>
+    <Container
+      ref={ref}
+      animate={controls}
+      initial="hidden"
+      variants={animationVariants as any}
+    >
       <Text
         type="h3"
         weight="thin"
